dd0c89fafbf667c7d5fe94d24f84a4d98fed202f
hzc-001 <2075070082@qq.com>
2021-10-25 15:58:59 +0800

    顶部和底部导航

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+     <router-view></router-view>
+    <main-tab-bar/>
+  </div>
+</template>
+
+<script>
+import MainTabBar from '@/components/content/mainTabbar/MainTabBarr';
+export default {
+  name: 'App',
+  components: {
+    MainTabBar,
+  },
+};
+</script>
+
+<style>
+ @import url(./assets/css/base.css);
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="home">
+    <nav-bar class="nav-bar">
+      <div slot="center">购物中心</div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/common/navbar/NavBar';
+export default {
+  components: { NavBar },
+  name: 'Home',
+};
+</script>
+
+<style>
+#home{
+  height: 100vh;
+}
+.nav-bar {
+  background-color: var(--color-tint);
+  font-weight:700;
+  color: #fff;
+  }
+</style>
